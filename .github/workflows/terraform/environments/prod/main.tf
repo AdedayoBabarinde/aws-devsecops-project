710f913edb73214d9de52474a0f293880e4f79b8
AdedayoBabarinde <dbabarinde@gmail.com>
2026-01-12 23:57:36 +0000

    update-infra-directory-structure

--- .github/workflows/terraform/environments/prod/main.tf
+++ .github/workflows/terraform/environments/prod/main.tf
@@ -20,7 +20,7 @@ terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket"
     key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
